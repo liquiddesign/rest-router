b7549f5ca361a94acf9a8ba0c098b085d7b89478
Oldrich <oldrich@lqd.cz>
2022-05-21 19:15:23 +0200

    toArray method

--- src/InputBody.php
+++ src/InputBody.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace REST;
 
+use Nette\Utils\Json;
+
 class InputBody extends \stdClass
 {
 	public function __construct(?\stdClass $payload = null)
@@ -23,5 +25,20 @@ class InputBody extends \stdClass
 	public function getAdditionalValidation(): array
 	{
 		return [];
+	}
+	
+	/**
+	 * @param array<string, mixed> $filterArrays
+	 * @return array<mixed, mixed>
+	 */
+	public function toArray(array $filterArrays = []): array
+	{
+		$result = Json::decode(Json::encode($this), Json::FORCE_ARRAY);
+		
+		foreach ($filterArrays as $name => $value) {
+			$result[$name] = \array_keys(\array_filter($result[$name], fn($val) => $val === $value));
+		}
+		
+		return $result;
 	}
 }
