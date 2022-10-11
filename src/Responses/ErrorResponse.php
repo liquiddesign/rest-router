d6c1771cff977c97fa19b0a8fc621c4a3f41cf93
Oldrich <oldrich@lqd.cz>
2022-10-11 14:42:39 +0200

    void allow

--- src/Responses/ErrorResponse.php
+++ src/Responses/ErrorResponse.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace REST\Responses;
 
+use Nette\Schema\ValidationException;
 use REST\Exception;
 
 class ErrorResponse extends JsonResponse
 {
 	public function __construct(\Throwable $exception, bool $debugMode)
 	{
-		$showError = $debugMode || $exception instanceof Exception;
+		$showError = $debugMode || $exception instanceof Exception || $exception instanceof ValidationException;
 		
 		$payload = [
 			'error' => $showError ? $exception->getMessage() : 'unspecified server error',
